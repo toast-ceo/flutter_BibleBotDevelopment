9ee34e84ced1053ec69db60a351ec0732ee02d03
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2020-11-19 17:24:27 +0900

    로그인ui

--- fifth_week_practice/lib/main.dart
+++ fifth_week_practice/lib/main.dart
@@ -2,15 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() =>runApp(MyApp());
+
+void main() => runApp(MyApp());
+
+final TextEditingController _idController = TextEditingController();
+final TextEditingController _passwordController = TextEditingController();
+final TextEditingController _pswdcheckController = TextEditingController();
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'fifth practice',
       initialRoute: '/',
-      routes: { '/':(context)=> FirstPage(),
+      routes: {
+        '/': (context) => FirstPage(),
+        '/second':(context) => SecondPage(),
+        '/third':(context) => ThirdPage(),
       },
     );
   }
@@ -24,62 +33,311 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   @override
 
-  final TextEditingController _myController = TextEditingController();
 
-  String id ='';
-  String password ='';
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset : false,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30.0),
-        child: Center(
+        child: Form(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 90.0,
+                ),
+                Text(
+                  "로그인",
+                  style: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "로그인하신 후 모든 서비스를 이용하실 수 있습니다",
+                  style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                TextFormField(
+                  controller: _idController,
+                  decoration: InputDecoration(
+                    labelText: "아이디 입력",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: "비밀번호 입력",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: MediaQuery.of(context).copyWith().size.height,
+                  height: 60,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(1.0),
+                        side: BorderSide(color: Colors.grey)),
+                    textColor: Colors.grey,
+                    color: Colors.white,
+                    child: Text('로그인'),
+                    onPressed: () {
+                    },
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 80,
+                    ),
+                    FlatButton(
+                      child:  Text(
+                        "회원가입",
+                        style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                      ),
+                      onPressed: (){
+                        Navigator.pushReplacementNamed(context, "/second");
+                      },
+                    ),
+                    Text(
+                      "|       비밀번호 찾기",
+                      style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 90.0,
+                ),
+                Text(
+                  "아이디로 회원가입",
+                  style: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "로그인은 최고 5자 이상으로 입력해주세요",
+                  style: TextStyle(fontSize: 14.0, color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                TextFormField(
+                  controller: _idController,
+                  decoration: InputDecoration(
+                    labelText: "아이디 입력",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: "비밀번호 입력",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _pswdcheckController,
+                  decoration: InputDecoration(
+                    labelText: "비밀번호 확인",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: MediaQuery.of(context).copyWith().size.height,
+                  height: 60,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(1.0),
+                        side: BorderSide(color: Colors.grey)),
+                    textColor: Colors.grey,
+                    color: Colors.white,
+                    child: Text('다음으로'),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, "/third");
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+  }
+}
+
+class ThirdPage extends StatefulWidget {
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal:45.0),
+        child: Form(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 90.0,),
-              Text("로그인",style: TextStyle(fontSize: 28.0,color: Colors.black, fontWeight: FontWeight.bold),),
-              SizedBox(height: 10.0,),
-              Text("로그인하신 후 모든 서비스를 이용하실 수 있습니다",style: TextStyle(fontSize: 12.0,color: Colors.grey),),
-              SizedBox(height: 30.0,),
+              SizedBox(
+                height: 90.0,
+              ),
+              Text(
+                "닉네임 입력",
+                style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 90.0,
+              ),
               TextFormField(
-                controller: _myController,
+                controller: _pswdcheckController,
                 decoration: InputDecoration(
-                  labelText: "아이디 입력",
+                  labelText: "닉네임 입력",
                   labelStyle: TextStyle(
                     color: Colors.grey,
                   ),
                   border: OutlineInputBorder(
-                    borderSide: BorderSide(color:Colors.amber,),
+                    borderSide: BorderSide(
+                      color: Colors.amber,
+                    ),
                     borderRadius: BorderRadius.circular(5),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
-                      color:Colors.amber,
+                      color: Colors.amber,
                     ),
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
               ),
-              SizedBox(height: 20,),
-              TextFormField(
-                controller: _myController,
-                decoration: InputDecoration(
-                  labelText: "비밀번호 입력",
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color:Colors.amber,),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color:Colors.amber,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
+              SizedBox(
+                height: 25,
               ),
-              SizedBox(height: 15,),
               Container(
                 width: MediaQuery.of(context).copyWith().size.height,
                 height: 60,
@@ -88,11 +346,12 @@ class _FirstPageState extends State<FirstPage> {
                       borderRadius: BorderRadius.circular(1.0),
                       side: BorderSide(color: Colors.grey)),
                   textColor: Colors.grey,
-                    color: Colors.white,
-                    child: Text('로그인'),
-                    onPressed: (){
-                    },
-                  ),
+                  color: Colors.white,
+                  child: Text('입력'),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, "/");
+                  },
+                ),
               ),
             ],
           ),
@@ -101,4 +360,3 @@ class _FirstPageState extends State<FirstPage> {
     );
   }
 }
-
