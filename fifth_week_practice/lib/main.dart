02e92eaf8f6b53ebee7438e87e0fc06612dfeddd
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2020-11-21 23:58:39 +0900

    로그인하는 방법
    appbar를 이용한 뒤로가기. 글자의 길이, 맞음의 여부에 따른 버튼 기능 활성화,suffixIcon을 이용하여 받은 문자를 clear하는 법, 회원가입을 했을 때,  fluttertoast 를 이용해서 안내를 해주는 법 등을 구현

--- fifth_week_practice/lib/main.dart
+++ fifth_week_practice/lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 
 void main() => runApp(MyApp());
 
-final TextEditingController _idController = TextEditingController();
-final TextEditingController _passwordController = TextEditingController();
-final TextEditingController _pswdcheckController = TextEditingController();
-
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'fifth practice',
       initialRoute: '/',
       routes: {
         '/': (context) => FirstPage(),
-        '/second':(context) => SecondPage(),
-        '/third':(context) => ThirdPage(),
+        '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(),
       },
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+        elevation: 0.0,
+          color: Colors.white,
+        )
+      ),
     );
   }
 }
@@ -32,11 +34,14 @@ class FirstPage extends StatefulWidget {
 
 class _FirstPageState extends State<FirstPage> {
   @override
-
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool action = false;
+  bool action1 = false;
 
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
+      resizeToAvoidBottomInset: false,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30.0),
         child: Form(
@@ -82,12 +87,26 @@ class _FirstPageState extends State<FirstPage> {
                       ),
                       borderRadius: BorderRadius.circular(5),
                     ),
+                    suffixIcon: IconButton(
+                      onPressed: () => _idController.clear(),
+                      color: Colors.grey,
+                      icon: Icon(Icons.clear),),
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value.isEmpty) {
+                        action = false;
+                      } else {
+                        action = true;
+                      }
+                    });
+                  },
                 ),
                 SizedBox(
                   height: 20,
                 ),
                 TextFormField(
+                  obscureText: true,
                   controller: _passwordController,
                   decoration: InputDecoration(
                     labelText: "비밀번호 입력",
@@ -106,7 +125,20 @@ class _FirstPageState extends State<FirstPage> {
                       ),
                       borderRadius: BorderRadius.circular(5),
                     ),
+                    suffixIcon: IconButton(
+                      onPressed: () => _passwordController.clear(),
+                      color: Colors.grey,
+                      icon: Icon(Icons.clear),),
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value.length > 4 && action1 == false) {
+                        action1 = true;
+                      } else {
+                        action1 = false;
+                      }
+                    });
+                  },
                 ),
                 SizedBox(
                   height: 25,
@@ -114,16 +146,27 @@ class _FirstPageState extends State<FirstPage> {
                 Container(
                   width: MediaQuery.of(context).copyWith().size.height,
                   height: 60,
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(1.0),
-                        side: BorderSide(color: Colors.grey)),
-                    textColor: Colors.grey,
-                    color: Colors.white,
-                    child: Text('로그인'),
-                    onPressed: () {
-                    },
-                  ),
+                  child: action1
+                      ? FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(1.0),
+                              side: BorderSide(color: Colors.grey)),
+                          textColor: Colors.white,
+                          color: Colors.amber,
+                          child: Text(
+                            '로그인',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          onPressed: () {},
+                        )
+                      : Container(
+                          color: Colors.white,
+                          child: Center(
+                              child: Text(
+                            '로그인',
+                            style: TextStyle(color: Colors.grey, fontSize: 16),
+                          )),
+                        ),
                 ),
                 Row(
                   children: <Widget>[
@@ -131,11 +174,11 @@ class _FirstPageState extends State<FirstPage> {
                       width: 80,
                     ),
                     FlatButton(
-                      child:  Text(
+                      child: Text(
                         "회원가입",
                         style: TextStyle(fontSize: 12.0, color: Colors.grey),
                       ),
-                      onPressed: (){
+                      onPressed: () {
                         Navigator.pushReplacementNamed(context, "/second");
                       },
                     ),
@@ -161,127 +204,164 @@ class SecondPage extends StatefulWidget {
 
 class _SecondPageState extends State<SecondPage> {
   @override
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _pswdcheckController = TextEditingController();
+  bool action = false;
+
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
+      appBar: AppBar(leading: IconButton(icon: Icon(Icons.arrow_back,color: Colors.grey,),onPressed: (){
+        Navigator.pushReplacementNamed(context, "/");
+      },)),
+      resizeToAvoidBottomInset: false,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30.0),
         child: Form(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 90.0,
-                ),
-                Text(
-                  "아이디로 회원가입",
-                  style: TextStyle(
-                      fontSize: 28.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  "로그인은 최고 5자 이상으로 입력해주세요",
-                  style: TextStyle(fontSize: 14.0, color: Colors.grey),
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                TextFormField(
-                  controller: _idController,
-                  decoration: InputDecoration(
-                    labelText: "아이디 입력",
-                    labelStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "아이디로 회원가입",
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "로그인은 최고 5자 이상으로 입력해주세요",
+                style: TextStyle(fontSize: 14.0, color: Colors.grey),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              TextFormField(
+                controller: _idController,
+                decoration: InputDecoration(
+                  labelText: "아이디 입력",
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: "비밀번호 입력",
-                    labelStyle: TextStyle(
-                      color: Colors.grey,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
                     ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
+                    borderRadius: BorderRadius.circular(5),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _pswdcheckController,
-                  decoration: InputDecoration(
-                    labelText: "비밀번호 확인",
-                    labelStyle: TextStyle(
-                      color: Colors.grey,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
                     ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
+                    borderRadius: BorderRadius.circular(5),
                   ),
+                  suffixIcon: IconButton(
+                    onPressed: () => _idController.clear(),
+                    color: Colors.grey,
+                    icon: Icon(Icons.clear),),
                 ),
-                SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  width: MediaQuery.of(context).copyWith().size.height,
-                  height: 60,
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(1.0),
-                        side: BorderSide(color: Colors.grey)),
-                    textColor: Colors.grey,
-                    color: Colors.white,
-                    child: Text('다음으로'),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, "/third");
-                    },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: "비밀번호 입력",
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
                   ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () => _passwordController.clear(),
+                    color: Colors.grey,
+                    icon: Icon(Icons.clear),),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: _pswdcheckController,
+                decoration: InputDecoration(
+                  labelText: "비밀번호 확인",
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.amber,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () => _pswdcheckController.clear(),
+                    color: Colors.grey,
+                    icon: Icon(Icons.clear),),
+                ),
+                onChanged: (value){
+                  setState(() {
+                    if(value == _passwordController.text && action == false){
+                      action = true;
+                    }else{
+                      action = false;
+                    }
+                  });
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: MediaQuery.of(context).copyWith().size.height,
+                height: 60,
+                child: action
+                    ? FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(1.0),
+                            side: BorderSide(color: Colors.grey)),
+                        textColor: Colors.white,
+                        color: Colors.amber,
+                        child: Text('다음으로',style: TextStyle(fontSize: 16),),
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(context, "/third");
+                        },
+                      )
+                    : Container(
+                        color: Colors.white,
+                        child: Center(
+                            child: Text(
+                          '다음으로',
+                          style: TextStyle(color: Colors.grey, fontSize: 16),
+                        )),
+                      ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
 
@@ -292,18 +372,23 @@ class ThirdPage extends StatefulWidget {
 
 class _ThirdPageState extends State<ThirdPage> {
   @override
+
+  final TextEditingController _nameController = TextEditingController();
+  bool action = false;
+
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(leading: IconButton(icon: Icon(Icons.arrow_back,color: Colors.grey,),onPressed: (){
+        Navigator.pushReplacementNamed(context, "/second");
+      },)),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal:45.0),
+        padding: EdgeInsets.symmetric(horizontal: 45.0),
         child: Form(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(
-                height: 90.0,
-              ),
+             
               Text(
                 "닉네임 입력",
                 style: TextStyle(
@@ -315,7 +400,7 @@ class _ThirdPageState extends State<ThirdPage> {
                 height: 90.0,
               ),
               TextFormField(
-                controller: _pswdcheckController,
+                controller: _nameController,
                 decoration: InputDecoration(
                   labelText: "닉네임 입력",
                   labelStyle: TextStyle(
@@ -333,7 +418,20 @@ class _ThirdPageState extends State<ThirdPage> {
                     ),
                     borderRadius: BorderRadius.circular(5),
                   ),
+                  suffixIcon: IconButton(
+                    onPressed: () => _nameController.clear(),
+                    color: Colors.grey,
+                    icon: Icon(Icons.clear),),
                 ),
+                onChanged: (value){
+                  setState(() {
+                    if(value.length > 0){
+                      action = true;
+                    }else{
+                      action = false;
+                    }
+                  });
+                },
               ),
               SizedBox(
                 height: 25,
@@ -341,16 +439,24 @@ class _ThirdPageState extends State<ThirdPage> {
               Container(
                 width: MediaQuery.of(context).copyWith().size.height,
                 height: 60,
-                child: FlatButton(
+                child: action ? FlatButton(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(1.0),
                       side: BorderSide(color: Colors.grey)),
-                  textColor: Colors.grey,
-                  color: Colors.white,
-                  child: Text('입력'),
+                  textColor: Colors.white,
+                  color: Colors.amber,
+                  child: Text('입력', style: TextStyle(fontSize: 16),),
                   onPressed: () {
+                    showToast();
                     Navigator.pushReplacementNamed(context, "/");
                   },
+                ) :  Container(
+                  color: Colors.white,
+                  child: Center(
+                      child: Text(
+                        '입력',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      )),
                 ),
               ),
             ],
@@ -359,4 +465,14 @@ class _ThirdPageState extends State<ThirdPage> {
       ),
     );
   }
+}
+
+void showToast() {
+  Fluttertoast.showToast(
+      msg: "회원가입이 완료되었습니다.",
+      gravity: ToastGravity.CENTER,
+      backgroundColor: Colors.black,
+      fontSize: 16.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_LONG);
 }
